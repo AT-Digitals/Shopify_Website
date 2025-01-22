f243615c649c6f9bff08f3378dd668a4ec0d75f4
Aishwarya Antony <aishu97.svpr@gmail.com>
2025-01-22 12:47:57 +0530

    new-all-page-changes

--- src/pages/Carrers/SeniorDeveloperPage.tsx
+++ src/pages/Carrers/SeniorDeveloperPage.tsx
@@ -1,3 +1,9 @@
+import { useEffect } from "react";
+import CustomDeveloperComponent from "../../Shared-fronend/CustomDeveloperComponent";
+
 export default function SeniorDeveloperPage() {
-  return <></>;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return <CustomDeveloperComponent jobTitle="Mid Front-end Developer" />;
 }
