37531e454d30f7bc76008eb659f35fd2b85db0a8
Dinesh <djeeva1515@gmail.com>
2025-01-22 10:45:15 +0530

    innerpage changes

--- src/pages/Carrers/JuniorDeveloperPage.tsx
+++ src/pages/Carrers/JuniorDeveloperPage.tsx
@@ -74,120 +74,50 @@ export default function JuniorDeveloperPage() {
     <Box
       sx={{
         background: "black",
-        width: "100%",
       }}
     >
-      <Box
-        sx={{
-          borderBottom: "1px solid lightgrey",
-          paddingBottom: "80px",
-        }}
-      >
-        <Box padding={"20px 30px"}>
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              color: "white",
-              fontSize: "20px",
-            }}
-          >
-            Full time | Varna, Bulgaria
-          </Typography>
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              fontSize: "50px",
-              fontWeight: "400",
-              color: "white",
-            }}
-          >
-            Junior Front End Developer
-          </Typography>
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              fontSize: "20px",
-              fontWeight: "400",
-              color: "white",
-              width: "100%",
-              maxWidth: "650px",
-            }}
-          >
-            CraftBerry is looking for a motivated Front-End Developer to join
-            our growing team of ambitious folks.
-          </Typography>
-        </Box>
-      </Box>
-      <Box
-        display={"flex"}
-        flexDirection={{ xs: "column", lg: "row" }}
-        justifyContent={"space-between"}
-        padding={{ xs: "30px 30px 50px", lg: "80px 30px 100px" }}
-        sx={{
-          borderBottom: "1px solid lightgrey",
-        }}
-      >
-        <Typography
-          fontFamily="Roboto"
+      <Box maxWidth={1300} margin={"auto"}>
+        <Box
           sx={{
-            fontSize: "50px",
-            fontWeight: "400",
-            color: "white",
+            borderBottom: "1px solid lightgrey",
+            paddingBottom: "80px",
           }}
         >
-          Summary
-        </Typography>
-        <Stack
-          flexDirection={"column"}
-          gap={"30px"}
-          sx={{ width: "100%", maxWidth: "850px" }}
-        >
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              fontSize: "20px",
-              color: "white",
-            }}
-          >
-            Are you a developer with a passion for clean and efficient code?
-            Would you enjoy building complex Shopify stores for internationally
-            recognized, sustainable, and ethical brands?
-          </Typography>
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              fontSize: "20px",
-              color: "white",
-            }}
-          >
-            At CraftBerry, we elevate premium eCommerce brands by designing and
-            building remarkable e-commerce experiences. We deliver modern-day
-            craftsmanship for international e-commerce brands that want to stand
-            out.
-          </Typography>
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              fontSize: "20px",
-              color: "white",
-            }}
-          >
-            We are looking for a bright, easygoing person who will join our
-            young and ambitious team and bring their energy and technical
-            knowledge to the company.
-          </Typography>
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              fontSize: "20px",
-              color: "white",
-            }}
-          >
-            The ideal candidate should have some front-end experience.
-          </Typography>
-        </Stack>
-      </Box>
-      {careers.map((task) => (
+          <Box padding={"20px 30px"}>
+            <Typography
+              fontFamily="Roboto"
+              sx={{
+                color: "white",
+                fontSize: "20px",
+              }}
+            >
+              Full time | Varna, Bulgaria
+            </Typography>
+            <Typography
+              fontFamily="Roboto"
+              sx={{
+                fontSize: "50px",
+                fontWeight: "400",
+                color: "white",
+              }}
+            >
+              Junior Front End Developer
+            </Typography>
+            <Typography
+              fontFamily="Roboto"
+              sx={{
+                fontSize: "20px",
+                fontWeight: "400",
+                color: "white",
+                width: "100%",
+                maxWidth: "650px",
+              }}
+            >
+              CraftBerry is looking for a motivated Front-End Developer to join
+              our growing team of ambitious folks.
+            </Typography>
+          </Box>
+        </Box>
         <Box
           display={"flex"}
           flexDirection={{ xs: "column", lg: "row" }}
@@ -205,152 +135,224 @@ export default function JuniorDeveloperPage() {
               color: "white",
             }}
           >
-            {task.heading}
+            Summary
           </Typography>
           <Stack
             flexDirection={"column"}
             gap={"30px"}
-            sx={{ width: "100%", maxWidth: "850px" }}
+            sx={{ width: { xs: "100%", sm: "100%", md: "50%" } }}
           >
             <Typography
               fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
+              sx={{
+                fontSize: "20px",
+                color: "white",
+              }}
             >
-              {task.task1}
+              Are you a developer with a passion for clean and efficient code?
+              Would you enjoy building complex Shopify stores for
+              internationally recognized, sustainable, and ethical brands?
             </Typography>
             <Typography
               fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
+              sx={{
+                fontSize: "20px",
+                color: "white",
+              }}
             >
-              {task.task2}
+              At CraftBerry, we elevate premium eCommerce brands by designing
+              and building remarkable e-commerce experiences. We deliver
+              modern-day craftsmanship for international e-commerce brands that
+              want to stand out.
             </Typography>
             <Typography
               fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
+              sx={{
+                fontSize: "20px",
+                color: "white",
+              }}
             >
-              {task.task3}
+              We are looking for a bright, easygoing person who will join our
+              young and ambitious team and bring their energy and technical
+              knowledge to the company.
             </Typography>
             <Typography
               fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
+              sx={{
+                fontSize: "20px",
+                color: "white",
+              }}
             >
-              {task.task4}
-            </Typography>
-            <Typography
-              fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
-            >
-              {task.task5}
-            </Typography>
-            <Typography
-              fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
-            >
-              {task.task6}
-            </Typography>
-            <Typography
-              fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
-            >
-              {task.task7}
-            </Typography>
-            <Typography
-              fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
-            >
-              {task.task8}
-            </Typography>
-            <Typography
-              fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
-            >
-              {task.task9}
-            </Typography>
-            <Typography
-              fontFamily="Roboto"
-              sx={{ fontSize: "20px", color: "white" }}
-            >
-              {task.task10}
+              The ideal candidate should have some front-end experience.
             </Typography>
           </Stack>
         </Box>
-      ))}
-      <Box
-        display={"flex"}
-        flexDirection={{ xs: "column", lg: "row" }}
-        justifyContent={"space-between"}
-        padding={{ xs: "30px 30px 50px", lg: "80px 30px 100px" }}
-        sx={{
-          borderBottom: "1px solid lightgrey",
-        }}
-      >
-        <Typography
-          fontFamily="Roboto"
-          sx={{
-            fontSize: "50px",
-            fontWeight: "400",
-            color: "white",
-          }}
-        >
-          What’s next?
-        </Typography>
-        <Stack
-          flexDirection={"column"}
-          gap={"30px"}
-          sx={{ width: "100%", maxWidth: "850px" }}
-        >
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              fontSize: "20px",
-              color: "white",
-            }}
-          >
-            If you are willing to work full-time mainly from our office in Varna
-            or remotely and see your profile as a good match for us even if you
-            do not cover all skill requirements, please send us your resume and
-            motivational letter to hello@craftberry.co.
-          </Typography>
-          <Typography
-            fontFamily="Roboto"
-            sx={{
-              fontSize: "20px",
-              color: "white",
-            }}
-          >
-            CraftBerry is an equal opportunity employer that supports diversity
-            and looks for professionals with cutting-edge skills and various
-            backgrounds. All applications will be treated in strict
-            confidentiality and without any form of discrimination.
-          </Typography>
-          <Typography
-            fontFamily="Roboto"
+        {careers.map((task) => (
+          <Box
+            key={task.heading}
             display={"flex"}
-            gap={"15px"}
+            flexDirection={{ xs: "column", lg: "row" }}
+            justifyContent={"space-between"}
+            padding={{ xs: "30px 30px 50px", lg: "80px 30px 100px" }}
             sx={{
-              fontSize: "20px",
-              color: "white",
+              borderBottom: "1px solid lightgrey",
             }}
           >
-            Email-us
-            <IconButton
+            <Typography
+              fontFamily="Roboto"
               sx={{
-                background: "rgb(184, 0, 64)",
+                fontSize: "50px",
+                fontWeight: "400",
                 color: "white",
-                width: "31px",
-                height: "31px",
-                transition: "width 0.3s ease",
-                ":hover": {
-                  background: "rgb(184, 0, 64)",
-                  width: "51px",
-                  borderRadius: "30px",
-                },
               }}
             >
-              <ChevronRightIcon />
-            </IconButton>
+              {task.heading}
+            </Typography>
+            <Stack
+              flexDirection={"column"}
+              gap={"30px"}
+              sx={{ width: { xs: "100%", sm: "100%", md: "50%" } }}
+            >
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task1}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task2}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task3}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task4}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task5}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task6}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task7}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task8}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task9}
+              </Typography>
+              <Typography
+                fontFamily="Roboto"
+                sx={{ fontSize: "20px", color: "white" }}
+              >
+                {task.task10}
+              </Typography>
+            </Stack>
+          </Box>
+        ))}
+        <Box
+          display={"flex"}
+          flexDirection={{ xs: "column", lg: "row" }}
+          justifyContent={"space-between"}
+          padding={{ xs: "30px 30px 50px", lg: "80px 30px 100px" }}
+          sx={{
+            borderBottom: "1px solid lightgrey",
+          }}
+        >
+          <Typography
+            fontFamily="Roboto"
+            sx={{
+              fontSize: "50px",
+              fontWeight: "400",
+              color: "white",
+            }}
+          >
+            What’s next?
           </Typography>
-        </Stack>
+          <Stack
+            flexDirection={"column"}
+            gap={"30px"}
+            sx={{ width: { xs: "100%", sm: "100%", md: "50%" } }}
+          >
+            <Typography
+              fontFamily="Roboto"
+              sx={{
+                fontSize: "20px",
+                color: "white",
+              }}
+            >
+              If you are willing to work full-time mainly from our office in
+              Varna or remotely and see your profile as a good match for us even
+              if you do not cover all skill requirements, please send us your
+              resume and motivational letter to hello@craftberry.co.
+            </Typography>
+            <Typography
+              fontFamily="Roboto"
+              sx={{
+                fontSize: "20px",
+                color: "white",
+              }}
+            >
+              CraftBerry is an equal opportunity employer that supports
+              diversity and looks for professionals with cutting-edge skills and
+              various backgrounds. All applications will be treated in strict
+              confidentiality and without any form of discrimination.
+            </Typography>
+            <Typography
+              fontFamily="Roboto"
+              display={"flex"}
+              gap={"15px"}
+              sx={{
+                fontSize: "20px",
+                color: "white",
+              }}
+            >
+              Email-us
+              <IconButton
+                sx={{
+                  background: "rgb(184, 0, 64)",
+                  color: "white",
+                  width: "31px",
+                  height: "31px",
+                  transition: "width 0.3s ease",
+                  ":hover": {
+                    background: "rgb(184, 0, 64)",
+                    width: "51px",
+                    borderRadius: "30px",
+                  },
+                }}
+              >
+                <ChevronRightIcon />
+              </IconButton>
+            </Typography>
+          </Stack>
+        </Box>
       </Box>
     </Box>
   );
